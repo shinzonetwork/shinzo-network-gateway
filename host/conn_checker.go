cc1baf89609e62142cf14b74ddc7c6a794cfb7f8
Tomasz Zdybał <tomek@zdybal.lap.pl>
2026-04-01 16:59:04 +0200

    feat: HttpConnectionChecker

--- host/conn_checker.go
+++ host/conn_checker.go
@@ -1,13 +1,15 @@
 package host
 
 import (
+	"context"
+	"net/http"
 	"time"
 
 	"go.uber.org/zap"
 )
 
 type ConnectionChecker interface {
-	CheckConnection(h Host) ConnectionStatus
+	CheckConnection(ctx context.Context, h Host) ConnectionStatus
 }
 
 type ConnectionStatus struct {
@@ -16,18 +18,36 @@ type ConnectionStatus struct {
 }
 
 type HttpConnectionChecker struct {
+	client *http.Client
 	logger *zap.Logger
 }
 
 var _ ConnectionChecker = &HttpConnectionChecker{}
 
-func (cc *HttpConnectionChecker) CheckConnection(h Host) ConnectionStatus {
-	start := time.Now()
+func NewHttpConnectionChecker(timeout time.Duration, logger *zap.Logger) *HttpConnectionChecker {
+	return &HttpConnectionChecker{
+		client: &http.Client{Timeout: timeout},
+		logger: logger.Named("connection-checker"),
+	}
+}
 
+func (cc *HttpConnectionChecker) CheckConnection(ctx context.Context, h Host) ConnectionStatus {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, string(h), nil)
+	if err != nil {
+		return ConnectionStatus{Online: false}
+	}
+	start := time.Now()
+	resp, err := cc.client.Do(req)
 	duration := time.Since(start)
 
+	if err != nil {
+		return ConnectionStatus{Online: false}
+	}
+	_ = resp.Body.Close()
+
+	cc.logger.Sugar().Debugw("host check result", "url", string(h), "status", resp.StatusCode, "rtt", duration)
 	return ConnectionStatus{
-		Online: true,
+		Online: resp.StatusCode < 400,
 		RTT:    duration,
 	}
 }
---
+++ host/conn_checker_test.go
@@ -0,0 +1,87 @@
+package host
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"go.uber.org/zap"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHttpConnectionChecker(t *testing.T) {
+	logger, err := zap.NewDevelopment()
+	require.NoError(t, err)
+	defer func() {
+		_ = logger.Sync()
+	}()
+
+	const timeout = 100 * time.Millisecond
+	const delay = timeout + 10*time.Millisecond
+
+	okServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	badServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+	}))
+
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	canceledCtx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	cases := []struct {
+		name string
+		url  string
+		ctx  context.Context
+		err  bool
+	}{
+		{
+			name: "OK",
+			url:  okServer.URL,
+			ctx:  context.Background(),
+			err:  false,
+		},
+		{
+			name: "not found",
+			url:  badServer.URL,
+			ctx:  context.Background(),
+			err:  true,
+		},
+		{
+			name: "timeoout",
+			url:  slowServer.URL,
+			ctx:  context.Background(),
+			err:  true,
+		},
+		{
+			name: "context canceled",
+			url:  okServer.URL,
+			ctx:  canceledCtx,
+			err:  true,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			cc := NewHttpConnectionChecker(timeout, logger)
+			status := cc.CheckConnection(c.ctx, Host(c.url))
+			if c.err {
+				assert.False(t, status.Online)
+			} else {
+				assert.True(t, status.Online)
+				assert.NotZero(t, status.RTT)
+				assert.LessOrEqual(t, status.RTT, timeout)
+			}
+		})
+	}
+}
