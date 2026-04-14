bf6847f8ac6edf9596d594525f0a6803ccae33b6
Tomasz Zdybał <tomek@zdybal.lap.pl>
2026-04-14 18:54:14 +0200

    test: GraphQL query parsing tests

--- endpoint/handler.go
+++ endpoint/handler.go
@@ -1,40 +1,51 @@
 package endpoint
 
 import (
+	"errors"
 	"io"
 	"net/http"
 	"sync"
 
 	"github.com/vektah/gqlparser/v2/ast"
 	"github.com/vektah/gqlparser/v2/parser"
+	"go.uber.org/zap"
 
 	"github.com/shinzonetwork/shinzo-network-gateway/host"
 )
 
+// ErrEmptyQuery is returned if GraphQL query is empty.
+var ErrEmptyQuery = errors.New("empty GraphQL query")
+
+// HostsSelector interface allows handler to get hosts for given collections.
 type HostsSelector interface {
 	SelectHosts(collections []string) ([]host.Host, error)
 }
 
+// Handler is a HTTP handler for "POST /graphql" endpoint.
 type Handler struct {
 	extractor CollectionsExtractor
 	selector  HostsSelector
+	logger    *zap.Logger
 }
 
 type hostResponse struct {
-	response string
+	response []byte
 	err      error
 }
 
 var _ http.Handler = &Handler{}
 
-func NewHandler(extractor CollectionsExtractor, selector HostsSelector) *Handler {
+// NewHandler creates new Handler instance.
+func NewHandler(extractor CollectionsExtractor, selector HostsSelector, logger *zap.Logger) *Handler {
 	return &Handler{
 		extractor: extractor,
 		selector:  selector,
+		logger:    logger.Named("handler"),
 	}
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.logger.Sugar().Debugw("serving HTTP request", "from", r.RemoteAddr)
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		// TODO(tzdybal): handle errors
@@ -60,7 +71,7 @@ func (h *Handler) getHostsReponses(hosts []host.Host, body []byte) []hostRespons
 	wg := &sync.WaitGroup{}
 	for i, host := range hosts {
 		wg.Go(func() {
-			responses[i] = h.queryHost(host, string(body))
+			responses[i] = h.queryHost(host, body)
 		})
 	}
 	wg.Wait()
@@ -68,7 +79,7 @@ func (h *Handler) getHostsReponses(hosts []host.Host, body []byte) []hostRespons
 	return responses
 }
 
-func (h *Handler) queryHost(host host.Host, body string) hostResponse {
+func (h *Handler) queryHost(host host.Host, body []byte) hostResponse {
 	panic("implement me!")
 }
 
@@ -86,12 +97,15 @@ type DefaultCollectionExtractor struct{}
 
 // ExtractCollections parses GraphQL into AST and then traverse to get the root collections.
 func (e *DefaultCollectionExtractor) ExtractCollections(graphql string) ([]string, error) {
+	if len(graphql) == 0 {
+		return nil, ErrEmptyQuery
+	}
 	query, err := parser.ParseQuery(&ast.Source{Input: graphql})
 	if err != nil {
 		return nil, err
 	}
 
-	rootCollections := make([]string, 1)
+	rootCollections := make([]string, 0, 1)
 	for _, op := range query.Operations {
 		for _, sel := range op.SelectionSet {
 			if field, ok := sel.(*ast.Field); ok {
