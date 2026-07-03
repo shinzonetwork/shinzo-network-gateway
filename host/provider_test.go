12a00b4de13bfebacf7dff4d5b26ff25e6e7b070
Tomasz Zdybał <tomek@zdybal.lap.pl>
2026-07-03 15:57:03 +0200

    test: simple test for shinzohub provider

--- host/provider_test.go
+++ host/provider_test.go
@@ -3,6 +3,9 @@ package host
 import (
 	"context"
 	"errors"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 	"time"
 
@@ -39,7 +42,13 @@ func (mock *MockProvider) Run(ctx context.Context, register func(Host), _ func(H
 	return nil
 }
 
+// SetLogger sets the logger used by the provider.
+func (mock *MockProvider) SetLogger(logger *zap.Logger) {
+	mock.logger = logger.Named("mock-provider")
+}
+
 func TestFileProvider(t *testing.T) {
+	t.Parallel()
 	logger, err := zap.NewDevelopment()
 	require.NoError(t, err)
 	defer func() {
@@ -61,7 +70,58 @@ func TestFileProvider(t *testing.T) {
 	require.Eventually(t, func() bool { return cnt > 1 }, 1*time.Second, 50*time.Millisecond)
 }
 
-// SetLogger sets the logger used by the provider.
-func (mock *MockProvider) SetLogger(logger *zap.Logger) {
-	mock.logger = logger.Named("mock-provider")
+func TestShinzohubProvider(t *testing.T) {
+	t.Parallel()
+
+	// this code is borrowed from shinzohub-api/client_test.go
+	const (
+		keyA = "c2hpbnpvMWRzZ2w4aDBuOWR5NnFnZGNxc3Bodzg2azNncjg0enBkeThka3Ew"
+		keyB = "c2hpbnpvMXY0cGp0bjN0a3J2NzNlcDNkamhmOGx5MjRmeTZtOHZkdnQ1ZmN6"
+	)
+	pages := map[string]string{
+		"":   "../pkg/shinzohub-api/testdata/hosts_page0.json",
+		keyA: "../pkg/shinzohub-api/testdata/hosts_page1.json",
+		keyB: "../pkg/shinzohub-api/testdata/hosts_page2.json",
+	}
+
+	var gotKeys []string
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		key := r.URL.Query().Get("pagination.key")
+		gotKeys = append(gotKeys, key)
+
+		file, ok := pages[key]
+		require.True(t, ok, "unexpected pagination.key %q", key)
+
+		body, err := os.ReadFile(file)
+		require.NoError(t, err)
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = w.Write(body)
+	}))
+	defer srv.Close()
+
+	logger, err := zap.NewDevelopment()
+	require.NoError(t, err)
+
+	provider := NewShinzohubProvider(srv.URL)
+	provider.SetLogger(logger)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancel()
+
+	var registered, deregistered []Host
+	register := func(h Host) {
+		registered = append(registered, h)
+	}
+
+	deregister := func(h Host) {
+		deregistered = append(deregistered, h)
+	}
+
+	provider.updateHosts(ctx, register, deregister)
+
+	require.Equal(t, 24, len(registered))
+	require.Equal(t, 0, len(deregistered))
+
+	// unfortunately, all hosts in testdata have the same endpoint address
+	require.Contains(t, registered, Host("https://192.168.1.1/api/v0/graphql"))
 }
