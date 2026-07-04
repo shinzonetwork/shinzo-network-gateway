679cd22db5a0ca8f8ac781951503c2cb43d2452e
Tomasz Zdybał <tomek@zdybal.lap.pl>
2026-07-04 18:32:30 +0200

    docs: add comment on Provider interface

--- host/provider.go
+++ host/provider.go
@@ -5,6 +5,7 @@ import (
 )
 
 // Provider calls register and deregister callbacks to notify when hosts are registered/deregistered.
+// Both callbacks must be non-nil.
 type Provider interface {
 	Run(ctx context.Context, register func(Host), deregister func(Host)) error
 }
