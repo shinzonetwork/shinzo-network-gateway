6d0127cbc1f1de39b1a7853934916ad2732cd3f8
Tomasz Zdybał <tomek@zdybal.lap.pl>
2026-07-04 19:43:43 +0200

    lint: remove old vars

--- host/collections_fetcher.go
+++ host/collections_fetcher.go
@@ -2,12 +2,7 @@ package host
 
 import (
 	"context"
-	"errors"
 )
-
-const collectionsPath = "/api/v0/collections"
-
-var errUnexpectedStatus = errors.New("unexpected HTTP status")
 
 // CollectionsFetcher retrieves the list of collections served by a host.
 type CollectionsFetcher interface {
